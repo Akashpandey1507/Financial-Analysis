170213037730cca7c25d94ad3dc0a8741b8818e6
Akash <124170332+Akashpandey1507@users.noreply.github.com>
2023-06-21 15:00:34 +0530

    Add files via upload
    Signed-off-by: Akash <124170332+Akashpandey1507@users.noreply.github.com>

---
+++ Financial Calculators/Calculate the EBITDA.py
@@ -0,0 +1,23 @@
+'''--------------------------------Canculate the EBITDA---------------------------------'''
+
+def Calculate_EBITDA():
+
+    # Need Net Income
+    Net_Income = float(input('Enter the Net Income: '))
+    # Need Interest
+    Interest = float(input('Enter the Net Interest: '))
+    # Need Taxes
+    Taxes = float(input('Enter the Net Tax: '))
+    # Need Depriciation
+    Depriciation = float(input('Enter the Net Depriciation: '))
+    # Need Amortization
+    Amortization = float(input('Enter the Net Amortization: '))
+
+    # Formula for calculating EBITDA
+    EBITDA = round(Net_Income + Interest + Taxes + Depriciation + Amortization)
+
+    # Print the Resut
+    return (f'The EBITDA is :Rs. {EBITDA}')
+
+
+print(Calculate_EBITDA())
